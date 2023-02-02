d3efe94c3a1379f0757b774c74f6ad31b5fedafb
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2023-02-03 00:34:20 +0530

    feat: update "Creational" patterns

---
+++ Creational/S6ObjectPool/ObjectPoolDemo.java
@@ -0,0 +1,14 @@
+package Creational.S6ObjectPool;
+
+// It's used for scenarios where creating object are expensive
+
+//Adv
+//-> Obj are cached, no need to create new objs all the time
+//-> Enforces objects reusability -> cached objects are returned from the pool
+
+//Disadv
+//-> complexity is high with this Design pattern
+//-> tends to rely on other patterns
+//-> Low popularity due to complexity
+public class ObjectPoolDemo {
+}
