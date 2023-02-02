d010842c0ca05077b772c6a44b1d9d98a62ea20d
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2023-02-03 00:29:31 +0530

    update comment

--- Creational/S5Prototype/PrototypeDemo.java
+++ Creational/S5Prototype/PrototypeDemo.java
@@ -5,5 +5,8 @@ package Creational.S5Prototype;
 
 //Usually the prototype pattern fits when your object is costly to create, & "cloning" object by copying
 // all it's member variables would do the trick
+
+//How ? Create new objects with Clone(), NOT new()
+//      Makes use of an interface that implements Clonable in order to clone your objects
 public class PrototypeDemo {
 }
